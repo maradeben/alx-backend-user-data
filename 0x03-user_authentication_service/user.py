9c238829e139f0627c22b2f504a780ea3e3b3099
Eben <maradebenezer@gmail.com>
2023-11-15 23:38:47 +0100

    initial commit, added initial user.py

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class for ORM """
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(length=250), nullable=False)
+    hashed_password = Column(String(length=250), nullable=False)
+    session_id = Column(String(length=250), nullable=True)
+    reset_token = Column(String(length=250), nullable=True)
